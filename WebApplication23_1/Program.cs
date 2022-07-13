cd204298a138ebb03d96316fac6c2c2204103282
Zaha_xk93@student.itstep.kz <Zaha_xk93@student.itstep.kz>
2022-07-13 15:26:28 +0600

    ALL

--- WebApplication23_1/Program.cs
+++ WebApplication23_1/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AdvertisementContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDbContext<AdvertisementContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
